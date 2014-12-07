be544a59e14e2d397f097c71ab0e23d01defc41f
Lidia <Lidia@Lidia-PC>
2014-12-07 18:29:00 -0400

    agregue unit test a numero natural

--- DemoArticuloLunes/src/demoarticulolunes/Negocio/NumeroNatural.java
+++ DemoArticuloLunes/src/demoarticulolunes/Negocio/NumeroNatural.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package demoarticulolunes.Negocio;
 
 /**
@@ -11,7 +10,8 @@ package demoarticulolunes.Negocio;
  * @author Lidia
  */
 public class NumeroNatural {
-   int numero;
+
+    int numero;
 
     public NumeroNatural(int numero) {
         this.numero = numero;
@@ -22,34 +22,55 @@ public class NumeroNatural {
     }
 
     public NumeroNatural() {
-        
+
     }
 
     public void setNumero(int numero) {
         this.numero = numero;
     }
-    public boolean Primo(){
-        if(numero ==1 )
-        {
+
+    public boolean Primo() {
+        if (numero == 1) {
             return false;
-        }
-        else{
+        } else {
             for (int i = 2; i <= numero; i++) {
-                   if (numero % i == 0) {
-                        if (numero == i)
-                        {
-                            return true;
-                        }
-                        else {
-                            return false;
-                        }
+                if (numero % i == 0) {
+                    if (numero == i) {
+                        return true;
+                    } else {
+                        return false;
                     }
+                }
             }
         }
         return false;
     }
+
     public Boolean Par() {
-            return numero % 2 == 0;
+        return numero % 2 == 0;
+    }
+
+    public int InvertirNumero() {
+        int numeroInvertido = 0, copia = this.getNumero(), resto;
+        while (copia != 0) {
+            resto = copia % 10;
+            numeroInvertido = numeroInvertido * 10 + resto;
+            copia = copia / 10;
         }
-    
+        return numeroInvertido;
+    }
+
+    public boolean EsCapicua() {
+        return this.getNumero() == InvertirNumero();
+    }
+
+    public int ContadorDeDigitos() {
+        int copia = this.getNumero(), contador = 0;
+        while (copia != 0) {
+            copia = copia / 10;
+            contador++;
+        }
+        return contador;
+    }
+
 }
